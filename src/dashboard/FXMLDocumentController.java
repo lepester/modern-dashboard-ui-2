e66dfaed618285d919c928bf94f9a9dc9ee1a7a3
Lepester <slavik55350@gmail.com>
2023-06-01 12:38:04 +0300

    added comments

--- src/dashboard/FXMLDocumentController.java
+++ src/dashboard/FXMLDocumentController.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dashboard;
 
 import com.jfoenix.controls.JFXButton;
@@ -20,10 +15,7 @@ import javafx.scene.Node;
 import javafx.scene.layout.AnchorPane;
 import javafx.util.Duration;
 
-/**
- *
- * @author danml
- */
+
 public class FXMLDocumentController implements Initializable {
 
     @FXML
